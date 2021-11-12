buu342/N64-Sausage64

commit 88e1aac90da83f7e460b0f964fa01bd72bfc9dfa

Fixed #10

Sample Parser/parser.c
@@ -87,7 +87,8 @@ void parse_sausage(FILE* fp)
         char* strdata;
         
         // Read a string from the text file
-        fgets(strbuf, STRBUFF_SIZE, fp);
+        if (fgets(strbuf, STRBUFF_SIZE, fp) == NULL)
+            terminate("Error: Problem reading s64 file\n");
         
         // Split the string by spaces
         strdata = strtok(strbuf, " ");

Sample Parser/dlist.c
@@ -143,12 +143,18 @@ static void offset_vertindices(linkedList* faces, int offset)
             // If this vertex index isn't in our hashtable yet
             if (hkeyval == NULL)
             {
+                #pragma GCC diagnostic push
+                #pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
                 hkeyval = htable_append(&vertstable, face->verts[i], (void*)(offset+vertcount)); // It's not a pointer, I'm lying
+                #pragma GCC diagnostic pop
                 vertcount++;
             }
             
             // Fix the vert index from the hashtable value
+            #pragma GCC diagnostic push
+            #pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
             face->verts[i] = (int)hkeyval->value;
+            #pragma GCC diagnostic pop
         }
     }
     
@@ -170,7 +176,7 @@ static void combine_caches(linkedList* vcachelist)
     listNode* firstnode, *secondnode;
     linkedList removedlist = EMPTY_LINKEDLIST;
     
-    // Iterate through all the nodes to check which can be combines
+    // Iterate through all the nodes to check which can be combined
     for (firstnode = vcachelist->head; firstnode != NULL; firstnode = firstnode->next)
     {
         vertCache* vc1 = (vertCache*)firstnode->data;
@@ -554,12 +560,18 @@ static linkedList* forsyth(vertCache* vcacheoriginal)
         // If this vertex index isn't in our hashtable yet, add it, and to our new vertex list
         if (hkeyval == NULL)
         {
+            #pragma GCC diagnostic push
+            #pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
             hkeyval = htable_append(&vertstable, outindices[i], (void*)(j++)); // It's not a pointer, I'm lying
+            #pragma GCC diagnostic pop
             list_append(&newvertorder, (s64Vert*)list_node_from_index(&vcacheoriginal->verts, outindices[i])->data);
         }
         
         // Fix the vert index from the hashtable value
+        #pragma GCC diagnostic push
+        #pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
         outindices[i] = (int) hkeyval->value;
+        #pragma GCC diagnostic pop
     }
     htable_destroy(&vertstable);
     
@@ -802,8 +814,6 @@ void construct_dl()
             check_splitverts(&vcachelist);
             
             // TODO: If that still didn't help (because we had an impossible to split model), then duplicate problem vertices and try Forsyth again
-            
-            // TODO: Sort by texture again to reduce display list commands
         }
         else
         {

Sample Parser/output.c
@@ -129,7 +129,7 @@ void write_output_text()
             s64Keyframe* keyf = (s64Keyframe*)keyfnode->data;
             fprintf(fp, "    {%d, anim_%s_%s_framedata%d},\n", keyf->keyframe, global_modelname, anim->name, keyf->keyframe);
         }
-        fprintf(fp, "};", global_modelname, anim->name);
+        fprintf(fp, "};");
     }
     
     // Finally, print the Sausage64 structs

Sample Parser/texture.c
@@ -10,6 +10,7 @@ Handles the parsing and creation of textures
 #include "main.h"
 #include "texture.h"
 
+
 /*********************************
               Macros
 *********************************/
@@ -145,6 +146,9 @@ n64Texture* find_texture(char* name)
     @returns A pointer to the new n64Texture 
 ==============================*/
 
+#pragma GCC diagnostic push 
+#pragma GCC diagnostic ignored "-Wunused-result" // TODO: Implement proper error checking
+
 n64Texture* parse_textures(FILE* fp)
 {
     n64Texture* tex;
@@ -256,9 +260,9 @@ n64Texture* request_texture(char* name)
     {
         case TYPE_TEXTURE:
             printf("\tTexture Width: ");
-            scanf("%d", &w); fflush(stdin);
+            scanf("%d", (int*)&w); fflush(stdin);
             printf("\tTexture Height: ");
-            scanf("%d", &h); fflush(stdin);
+            scanf("%d", (int*)&h); fflush(stdin);
             tex = add_texture(name, w, h);
             printf("\tTexture flags (separate by spaces): ");
             fgets(strbuf, STRBUFF_SIZE, stdin);
@@ -268,11 +272,11 @@ n64Texture* request_texture(char* name)
             break;
         case TYPE_PRIMCOL:
             printf("\tPrimitve Red: ");
-            scanf("%d", &r); fflush(stdin);
+            scanf("%d", (int*)&r); fflush(stdin);
             printf("\tPrimitve Green: ");
-            scanf("%d", &g); fflush(stdin);
+            scanf("%d", (int*)&g); fflush(stdin);
             printf("\tPrimitve Blue: ");
-            scanf("%d", &b); fflush(stdin);
+            scanf("%d", (int*)&b); fflush(stdin);
             tex = add_primcol(name, r, g, b);
             printf("\tTexture flags (separate by spaces): ");
             fgets(strbuf, STRBUFF_SIZE, stdin);
@@ -296,7 +300,7 @@ n64Texture* request_texture(char* name)
     }
     return tex;
 }
-
+#pragma GCC diagnostic pop
 
 /*==============================
     tex_setflag

commit 0e95a97788bfc2670960a0a7df17d76f7a096293

Fixed a bug regarding EOF introduced by 88e1aac

Sample Parser/parser.c
@@ -87,7 +87,7 @@ void parse_sausage(FILE* fp)
         char* strdata;
         
         // Read a string from the text file
-        if (fgets(strbuf, STRBUFF_SIZE, fp) == NULL)
+        if (fgets(strbuf, STRBUFF_SIZE, fp) == NULL && !feof(fp))
             terminate("Error: Problem reading s64 file\n");
         
         // Split the string by spaces
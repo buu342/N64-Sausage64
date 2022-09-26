1bfa88a95434747103f60a842a6618792f18e29d
Buu342 <buu342@hotmail.com>
2022-09-26 15:23:20 +0100

    Corrected Windows makeme batch file

--- Sample Parser/makeme.bat
+++ Sample Parser/makeme.bat
@@ -1,4 +1,1 @@
 gcc -O3 -o arabiki64.exe main.c datastructs.c mesh.c texture.c animation.c parser.c optimizer.c dlist.c opengl.c output.c
-pause
-arabiki64.exe -g -f sample/CatherineExported.S64 -t sample/CatherineTextures.txt
-pause

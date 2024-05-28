127ec92401d4a66aa25d22ebdf559b0a04329094
Buu342 <buu342@hotmail.com>
2024-05-28 04:17:47 +0100

    And more work on #39

--- Sample Parser/dlist.c
+++ Sample Parser/dlist.c
@@ -14,7 +14,6 @@ Constructs a display list string for outputting later.
 #include "mesh.h"
 #include "dlist.h"
 
-
 /*********************************
               Macros
 *********************************/
@@ -136,6 +135,262 @@ const DListCommand commands_f3dex2[] = {
     {SPSelectBranchDL, "SPSelectBranchDL", 4, 2},
 };
 
+// List of valid macros as of F3DEX2
+const GBIMacros macros_f3dex2[] = {
+    {enum_G_MTX_MODELVIEW, "G_MTX_MODELVIEW", 0x00},
+    {enum_G_MTX_PROJECTION, "G_MTX_PROJECTION", 0x04},
+    {enum_G_MTX_MUL, "G_MTX_MUL", 0x00},
+    {enum_G_MTX_LOAD, "G_MTX_LOAD", 0x02},
+    {enum_G_MTX_NOPUSH, "G_MTX_NOPUSH", 0x00},
+    {enum_G_MTX_PUSH, "G_MTX_PUSH", 0x01},
+    {enum_G_ZBUFFER, "G_ZBUFFER", 0x00000001},
+    {enum_G_SHADE, "G_SHADE", 0x00000004},
+    {enum_G_TEXTURE_ENABLE, "G_TEXTURE_ENABLE", 0x00000000},
+    {enum_G_SHADING_SMOOTH, "G_SHADING_SMOOTH", 0x00200000},
+    {enum_G_CULL_FRONT, "G_CULL_FRONT", 0x00000200},
+    {enum_G_CULL_BACK, "G_CULL_BACK", 0x00000400},
+    {enum_G_CULL_BOTH, "G_CULL_BOTH", 0x00000600},
+    {enum_G_FOG, "G_FOG", 0x00010000},
+    {enum_G_LIGHTING, "G_LIGHTING", 0x00020000},
+    {enum_G_TEXTURE_GEN, "G_TEXTURE_GEN", 0x00040000},
+    {enum_G_TEXTURE_GEN_LINEAR, "G_TEXTURE_GEN_LINEAR", 0x00080000},
+    {enum_G_LOD, "G_LOD", 0x00100000},
+    {enum_G_CLIPPING, "G_CLIPPING", 0x00800000},
+    {enum_G_TX_LOADTILE, "G_TX_LOADTILE", 7},
+    {enum_G_TX_RENDERTILE, "G_TX_RENDERTILE", 0},
+    {enum_G_TX_NOMIRROR, "G_TX_NOMIRROR", 0},
+    {enum_G_TX_WRAP, "G_TX_WRAP", 0},
+    {enum_G_TX_MIRROR, "G_TX_MIRROR", 0x1},
+    {enum_G_TX_CLAMP, "G_TX_CLAMP", 0x2},
+    {enum_G_TX_NOMASK, "G_TX_NOMASK", 0},
+    {enum_G_TX_NOLOD, "G_TX_NOLOD", 0},
+    {enum_G_IM_FMT_RGBA, "G_IM_FMT_RGBA", 0},
+    {enum_G_IM_FMT_YUV, "G_IM_FMT_YUV", 1},
+    {enum_G_IM_FMT_CI, "G_IM_FMT_CI", 2},
+    {enum_G_IM_FMT_IA, "G_IM_FMT_IA", 3},
+    {enum_G_IM_FMT_I, "G_IM_FMT_I", 4},
+    {enum_G_IM_SIZ_4b, "G_IM_SIZ_4b", 0},
+    {enum_G_IM_SIZ_8b, "G_IM_SIZ_8b", 1},
+    {enum_G_IM_SIZ_16b, "G_IM_SIZ_16b", 2},
+    {enum_G_IM_SIZ_32b, "G_IM_SIZ_32b", 3},
+    {enum_G_PM_1PRIMITIVE, "G_PM_1PRIMITIVE", (1 << 23)},
+    {enum_G_PM_NPRIMITIVE, "G_PM_NPRIMITIVE", (0 << 23)},
+    {enum_G_CYC_1CYCLE, "G_CYC_1CYCLE", (0 << 20)},
+    {enum_G_CYC_2CYCLE, "G_CYC_2CYCLE", (1 << 20)},
+    {enum_G_CYC_COPY, "G_CYC_COPY", (2 << 20)},
+    {enum_G_CYC_FILL, "G_CYC_FILL", (3 << 20)},
+    {enum_G_TP_NONE, "G_TP_NONE", (0 << 19)},
+    {enum_G_TP_PERSP, "G_TP_PERSP", (1 << 19)},
+    {enum_G_TD_CLAMP, "G_TD_CLAMP", (0 << 17)},
+    {enum_G_TD_SHARPEN, "G_TD_SHARPEN", (1 << 17)},
+    {enum_G_TD_DETAIL, "G_TD_DETAIL", (2 << 17)},
+    {enum_G_TL_TILE, "G_TL_TILE", (0 << 16)},
+    {enum_G_TL_LOD, "G_TL_LOD", (1 << 16)},
+    {enum_G_TT_NONE, "G_TT_NONE", (0 << 14)},
+    {enum_G_TT_RGBA16, "G_TT_RGBA16", (2 << 14)},
+    {enum_G_TT_IA16, "G_TT_IA16", (3 << 14)},
+    {enum_G_TF_POINT, "G_TF_POINT", (0 << 12)},
+    {enum_G_TF_AVERAGE, "G_TF_AVERAGE", (3 << 12)},
+    {enum_G_TF_BILERP, "G_TF_BILERP", (2 << 12)},
+    {enum_G_TC_CONV, "G_TC_CONV", (0 << 9)},
+    {enum_G_TC_FILTCONV, "G_TC_FILTCONV", (5 << 9)},
+    {enum_G_TC_FILT, "G_TC_FILT", (6 << 9)},
+    {enum_G_CK_NONE, "G_CK_NONE", (0 << 8)},
+    {enum_G_CK_KEY, "G_CK_KEY", (1 << 8)},
+    {enum_G_CD_MAGICSQ, "G_CD_MAGICSQ", (0 << 6)},
+    {enum_G_CD_BAYER, "G_CD_BAYER", (1 << 6)},
+    {enum_G_CD_NOISE, "G_CD_NOISE", (2 << 6)},
+    {enum_G_CD_ENABLE, "G_CD_ENABLE", (1 << 2)},
+    {enum_G_CD_DISABLE, "G_CD_DISABLE", (0 << 2)},
+    {enum_G_AD_PATTERN, "G_AD_PATTERN", (0 << 4)},
+    {enum_G_AD_NOTPATTERN, "G_AD_NOTPATTERN", (1 << 4)},
+    {enum_G_AD_NOISE, "G_AD_NOISE", (2 << 4)},
+    {enum_G_AD_DISABLE, "G_AD_DISABLE", (3 << 4)},
+    {enum_G_AC_NONE, "G_AC_NONE", (0 << 0)},
+    {enum_G_AC_THRESHOLD, "G_AC_THRESHOLD", (1 << 0)},
+    {enum_G_AC_DITHER, "G_AC_DITHER", (3 << 0)},
+    {enum_G_ZS_PIXEL, "G_ZS_PIXEL", (0 << 2)},
+    {enum_G_ZS_PRIM, "G_ZS_PRIM", (1 << 2)},
+    {enum_G_CV_K0, "G_CV_K0", 175},
+    {enum_G_CV_K1, "G_CV_K1", -43},
+    {enum_G_CV_K2, "G_CV_K2", -89},
+    {enum_G_CV_K3, "G_CV_K3", 222},
+    {enum_G_CV_K4, "G_CV_K4", 114},
+    {enum_G_CV_K5, "G_CV_K5", 42},
+    {enum_G_SC_NON_INTERLACE, "G_SC_NON_INTERLACE", 0},
+    {enum_G_SC_ODD_INTERLACE, "G_SC_ODD_INTERLACE", 3},
+    {enum_G_SC_EVEN_INTERLACE, "G_SC_EVEN_INTERLACE", 2},
+
+    {enum_G_CC_PRIMITIVE, "G_CC_PRIMITIVE", 0},
+    {enum_G_CC_SHADE, "G_CC_SHADE", 1},
+    {enum_G_CC_MODULATEI, "G_CC_MODULATEI", 2},
+    {enum_G_CC_MODULATEIA, "G_CC_MODULATEIA", 3},
+    {enum_G_CC_MODULATEIDECALA, "G_CC_MODULATEIDECALA", 4},
+    {enum_G_CC_MODULATERGB, "G_CC_MODULATERGB", 5},
+    {enum_G_CC_MODULATERGBA, "G_CC_MODULATERGBA", 6},
+    {enum_G_CC_MODULATERGBDECALA, "G_CC_MODULATERGBDECALA", 7},
+    {enum_G_CC_MODULATEI_PRIM, "G_CC_MODULATEI_PRIM", 8},
+    {enum_G_CC_MODULATEIA_PRIM, "G_CC_MODULATEIA_PRIM", 9},
+    {enum_G_CC_MODULATEIDECALA_PRIM, "G_CC_MODULATEIDECALA_PRIM", 10},
+    {enum_G_CC_MODULATERGB_PRIM, "G_CC_MODULATERGB_PRIM", 11},
+    {enum_G_CC_MODULATERGBA_PRIM, "G_CC_MODULATERGBA_PRIM", 12},
+    {enum_G_CC_MODULATERGBDECALA_PRIM, "G_CC_MODULATERGBDECALA_PRIM", 13},
+    {enum_G_CC_DECALRGB, "G_CC_DECALRGB", 14},
+    {enum_G_CC_DECALRGBA, "G_CC_DECALRGBA", 15},
+    {enum_G_CC_BLENDI, "G_CC_BLENDI", 16},
+    {enum_G_CC_BLENDIA, "G_CC_BLENDIA", 17},
+    {enum_G_CC_BLENDIDECALA, "G_CC_BLENDIDECALA", 18},
+    {enum_G_CC_BLENDRGBA, "G_CC_BLENDRGBA", 19},
+    {enum_G_CC_BLENDRGBDECALA, "G_CC_BLENDRGBDECALA", 20},
+    {enum_G_CC_ADDRGB, "G_CC_ADDRGB", 21},
+    {enum_G_CC_ADDRGBDECALA, "G_CC_ADDRGBDECALA", 22},
+    {enum_G_CC_REFLECTRGB, "G_CC_REFLECTRGB", 23},
+    {enum_G_CC_REFLECTRGBDECALA, "G_CC_REFLECTRGBDECALA", 24},
+    {enum_G_CC_HILITERGB, "G_CC_HILITERGB", 25},
+    {enum_G_CC_HILITERGBA, "G_CC_HILITERGBA", 26},
+    {enum_G_CC_HILITERGBDECALA, "G_CC_HILITERGBDECALA", 27},
+    {enum_G_CC_SHADEDECALA, "G_CC_SHADEDECALA", 28},
+    {enum_G_CC_BLENDPE, "G_CC_BLENDPE", 29},
+    {enum_G_CC_BLENDPEDECALA, "G_CC_BLENDPEDECALA", 30},
+    {enum__G_CC_BLENDPE, "_G_CC_BLENDPE", 31},
+    {enum__G_CC_BLENDPEDECALA, "_G_CC_BLENDPEDECALA", 32},
+    {enum__G_CC_TWOCOLORTEX, "_G_CC_TWOCOLORTEX", 33},
+    {enum__G_CC_SPARSEST, "_G_CC_SPARSEST", 34},
+    {enum_G_CC_TEMPLERP, "G_CC_TEMPLERP", 35},
+    {enum_G_CC_TRILERP, "G_CC_TRILERP", 36},
+    {enum_G_CC_INTERFERENCE, "G_CC_INTERFERENCE", 37},
+    {enum_G_CC_1CYUV2RGB, "G_CC_1CYUV2RGB", 38},
+    {enum_G_CC_YUV2RGB, "G_CC_YUV2RGB", 39},
+    {enum_G_CC_PASS2, "G_CC_PASS2", 40},
+    {enum_G_CC_MODULATEI2, "G_CC_MODULATEI2", 41},
+    {enum_G_CC_MODULATEIA2, "G_CC_MODULATEIA2", 42},
+    {enum_G_CC_MODULATERGB2, "G_CC_MODULATERGB2", 43},
+    {enum_G_CC_MODULATERGBA2, "G_CC_MODULATERGBA2", 44},
+    {enum_G_CC_MODULATEI_PRIM2, "G_CC_MODULATEI_PRIM2", 45},
+    {enum_G_CC_MODULATEIA_PRIM2, "G_CC_MODULATEIA_PRIM2", 46},
+    {enum_G_CC_MODULATERGB_PRIM2, "G_CC_MODULATERGB_PRIM2", 47},
+    {enum_G_CC_MODULATERGBA_PRIM2, "G_CC_MODULATERGBA_PRIM2", 48},
+    {enum_G_CC_DECALRGB2, "G_CC_DECALRGB2", 49},
+    {enum_G_CC_BLENDI2, "G_CC_BLENDI2", 50},
+    {enum_G_CC_BLENDIA2, "G_CC_BLENDIA2", 51},
+    {enum_G_CC_CHROMA_KEY2, "G_CC_CHROMA_KEY2", 52},
+    {enum_G_CC_HILITERGB2, "G_CC_HILITERGB2", 53},
+    {enum_G_CC_HILITERGBA2, "G_CC_HILITERGBA2", 54},
+    {enum_G_CC_HILITERGBDECALA2, "G_CC_HILITERGBDECALA2", 55},
+    {enum_G_CC_HILITERGBPASSA2, "G_CC_HILITERGBPASSA2", 56},
+    {enum_G_CC_PRIMLITE, "G_CC_PRIMLITE", 57},
+
+    {enum_G_RM_AA_ZB_OPA_SURF, "G_RM_AA_ZB_OPA_SURF", 0},
+    {enum_G_RM_AA_ZB_OPA_SURF2, "G_RM_AA_ZB_OPA_SURF2", 1},
+    {enum_G_RM_AA_ZB_XLU_SURF, "G_RM_AA_ZB_XLU_SURF", 2},
+    {enum_G_RM_AA_ZB_XLU_SURF2, "G_RM_AA_ZB_XLU_SURF2", 3},
+    {enum_G_RM_AA_ZB_OPA_DECAL, "G_RM_AA_ZB_OPA_DECAL", 4},
+    {enum_G_RM_AA_ZB_OPA_DECAL2, "G_RM_AA_ZB_OPA_DECAL2", 5},
+    {enum_G_RM_AA_ZB_XLU_DECAL, "G_RM_AA_ZB_XLU_DECAL", 6},
+    {enum_G_RM_AA_ZB_XLU_DECAL2, "G_RM_AA_ZB_XLU_DECAL2", 7},
+    {enum_G_RM_AA_ZB_OPA_INTER, "G_RM_AA_ZB_OPA_INTER", 8},
+    {enum_G_RM_AA_ZB_OPA_INTER2, "G_RM_AA_ZB_OPA_INTER2", 9},
+    {enum_G_RM_AA_ZB_XLU_INTER, "G_RM_AA_ZB_XLU_INTER", 10},
+    {enum_G_RM_AA_ZB_XLU_INTER2, "G_RM_AA_ZB_XLU_INTER2", 11},
+    {enum_G_RM_AA_ZB_XLU_LINE, "G_RM_AA_ZB_XLU_LINE", 12},
+    {enum_G_RM_AA_ZB_XLU_LINE2, "G_RM_AA_ZB_XLU_LINE2", 13},
+    {enum_G_RM_AA_ZB_DEC_LINE, "G_RM_AA_ZB_DEC_LINE", 14},
+    {enum_G_RM_AA_ZB_DEC_LINE2, "G_RM_AA_ZB_DEC_LINE2", 15},
+    {enum_G_RM_AA_ZB_TEX_EDGE, "G_RM_AA_ZB_TEX_EDGE", 16},
+    {enum_G_RM_AA_ZB_TEX_EDGE2, "G_RM_AA_ZB_TEX_EDGE2", 17},
+    {enum_G_RM_AA_ZB_TEX_INTER, "G_RM_AA_ZB_TEX_INTER", 18},
+    {enum_G_RM_AA_ZB_TEX_INTER2, "G_RM_AA_ZB_TEX_INTER2", 19},
+    {enum_G_RM_AA_ZB_SUB_SURF, "G_RM_AA_ZB_SUB_SURF", 20},
+    {enum_G_RM_AA_ZB_SUB_SURF2, "G_RM_AA_ZB_SUB_SURF2", 21},
+    {enum_G_RM_AA_ZB_PCL_SURF, "G_RM_AA_ZB_PCL_SURF", 22},
+    {enum_G_RM_AA_ZB_PCL_SURF2, "G_RM_AA_ZB_PCL_SURF2", 23},
+    {enum_G_RM_AA_ZB_OPA_TERR, "G_RM_AA_ZB_OPA_TERR", 24},
+    {enum_G_RM_AA_ZB_OPA_TERR2, "G_RM_AA_ZB_OPA_TERR2", 25},
+    {enum_G_RM_AA_ZB_TEX_TERR, "G_RM_AA_ZB_TEX_TERR", 26},
+    {enum_G_RM_AA_ZB_TEX_TERR2, "G_RM_AA_ZB_TEX_TERR2", 27},
+    {enum_G_RM_AA_ZB_SUB_TERR, "G_RM_AA_ZB_SUB_TERR", 28},
+    {enum_G_RM_AA_ZB_SUB_TERR2, "G_RM_AA_ZB_SUB_TERR2", 29},
+    {enum_G_RM_RA_ZB_OPA_SURF, "G_RM_RA_ZB_OPA_SURF", 30},
+    {enum_G_RM_RA_ZB_OPA_SURF2, "G_RM_RA_ZB_OPA_SURF2", 31},
+    {enum_G_RM_RA_ZB_OPA_DECAL, "G_RM_RA_ZB_OPA_DECAL", 32},
+    {enum_G_RM_RA_ZB_OPA_DECAL2, "G_RM_RA_ZB_OPA_DECAL2", 33},
+    {enum_G_RM_RA_ZB_OPA_INTER, "G_RM_RA_ZB_OPA_INTER", 34},
+    {enum_G_RM_RA_ZB_OPA_INTER2, "G_RM_RA_ZB_OPA_INTER2", 35},
+    {enum_G_RM_AA_OPA_SURF, "G_RM_AA_OPA_SURF", 36},
+    {enum_G_RM_AA_OPA_SURF2, "G_RM_AA_OPA_SURF2", 37},
+    {enum_G_RM_AA_XLU_SURF, "G_RM_AA_XLU_SURF", 38},
+    {enum_G_RM_AA_XLU_SURF2, "G_RM_AA_XLU_SURF2", 39},
+    {enum_G_RM_AA_XLU_LINE, "G_RM_AA_XLU_LINE", 40},
+    {enum_G_RM_AA_XLU_LINE2, "G_RM_AA_XLU_LINE2", 41},
+    {enum_G_RM_AA_DEC_LINE, "G_RM_AA_DEC_LINE", 42},
+    {enum_G_RM_AA_DEC_LINE2, "G_RM_AA_DEC_LINE2", 43},
+    {enum_G_RM_AA_TEX_EDGE, "G_RM_AA_TEX_EDGE", 44},
+    {enum_G_RM_AA_TEX_EDGE2, "G_RM_AA_TEX_EDGE2", 45},
+    {enum_G_RM_AA_SUB_SURF, "G_RM_AA_SUB_SURF", 46},
+    {enum_G_RM_AA_SUB_SURF2, "G_RM_AA_SUB_SURF2", 47},
+    {enum_G_RM_AA_PCL_SURF, "G_RM_AA_PCL_SURF", 48},
+    {enum_G_RM_AA_PCL_SURF2, "G_RM_AA_PCL_SURF2", 49},
+    {enum_G_RM_AA_OPA_TERR, "G_RM_AA_OPA_TERR", 50},
+    {enum_G_RM_AA_OPA_TERR2, "G_RM_AA_OPA_TERR2", 51},
+    {enum_G_RM_AA_TEX_TERR, "G_RM_AA_TEX_TERR", 52},
+    {enum_G_RM_AA_TEX_TERR2, "G_RM_AA_TEX_TERR2", 53},
+    {enum_G_RM_AA_SUB_TERR, "G_RM_AA_SUB_TERR", 54},
+    {enum_G_RM_AA_SUB_TERR2, "G_RM_AA_SUB_TERR2", 55},
+    {enum_G_RM_RA_OPA_SURF, "G_RM_RA_OPA_SURF", 56},
+    {enum_G_RM_RA_OPA_SURF2, "G_RM_RA_OPA_SURF2", 57},
+    {enum_G_RM_ZB_OPA_SURF, "G_RM_ZB_OPA_SURF", 58},
+    {enum_G_RM_ZB_OPA_SURF2, "G_RM_ZB_OPA_SURF2", 59},
+    {enum_G_RM_ZB_XLU_SURF, "G_RM_ZB_XLU_SURF", 60},
+    {enum_G_RM_ZB_XLU_SURF2, "G_RM_ZB_XLU_SURF2", 61},
+    {enum_G_RM_ZB_OPA_DECAL, "G_RM_ZB_OPA_DECAL", 62},
+    {enum_G_RM_ZB_OPA_DECAL2, "G_RM_ZB_OPA_DECAL2", 63},
+    {enum_G_RM_ZB_XLU_DECAL, "G_RM_ZB_XLU_DECAL", 64},
+    {enum_G_RM_ZB_XLU_DECAL2, "G_RM_ZB_XLU_DECAL2", 65},
+    {enum_G_RM_ZB_CLD_SURF, "G_RM_ZB_CLD_SURF", 66},
+    {enum_G_RM_ZB_CLD_SURF2, "G_RM_ZB_CLD_SURF2", 67},
+    {enum_G_RM_ZB_OVL_SURF, "G_RM_ZB_OVL_SURF", 68},
+    {enum_G_RM_ZB_OVL_SURF2, "G_RM_ZB_OVL_SURF2", 69},
+    {enum_G_RM_ZB_PCL_SURF, "G_RM_ZB_PCL_SURF", 70},
+    {enum_G_RM_ZB_PCL_SURF2, "G_RM_ZB_PCL_SURF2", 71},
+    {enum_G_RM_OPA_SURF, "G_RM_OPA_SURF", 72},
+    {enum_G_RM_OPA_SURF2, "G_RM_OPA_SURF2", 73},
+    {enum_G_RM_XLU_SURF, "G_RM_XLU_SURF", 74},
+    {enum_G_RM_XLU_SURF2, "G_RM_XLU_SURF2", 75},
+    {enum_G_RM_CLD_SURF, "G_RM_CLD_SURF", 76},
+    {enum_G_RM_CLD_SURF2, "G_RM_CLD_SURF2", 77},
+    {enum_G_RM_TEX_EDGE, "G_RM_TEX_EDGE", 78},
+    {enum_G_RM_TEX_EDGE2, "G_RM_TEX_EDGE2", 79},
+    {enum_G_RM_PCL_SURF, "G_RM_PCL_SURF", 80},
+    {enum_G_RM_PCL_SURF2, "G_RM_PCL_SURF2", 81},
+    {enum_G_RM_ADD, "G_RM_ADD", 82},
+    {enum_G_RM_ADD2, "G_RM_ADD2", 83},
+    {enum_G_RM_NOOP, "G_RM_NOOP", 84},
+    {enum_G_RM_NOOP2, "G_RM_NOOP2", 85},
+    {enum_G_RM_VISCVG, "G_RM_VISCVG", 86},
+    {enum_G_RM_VISCVG2, "G_RM_VISCVG2", 87},
+    {enum_G_RM_OPA_CI, "G_RM_OPA_CI", 88},
+    {enum_G_RM_OPA_CI2, "G_RM_OPA_CI2", 89},
+    {enum_G_RM_FOG_SHADE_A, "G_RM_FOG_SHADE_A", 90},
+    {enum_G_RM_FOG_PRIM_A, "G_RM_FOG_PRIM_A", 91},
+    {enum_G_RM_PASS, "G_RM_PASS", 92},
+};
+
+const DListCName supported_binary[] = {
+    DPSetCycleType,
+    DPSetRenderMode,
+    DPSetCombineMode,
+    DPSetTextureFilter,
+    SPClearGeometryMode,
+    SPSetGeometryMode,
+    DPLoadTextureBlock,
+    DPLoadTextureBlock_4b,
+    DPSetPrimColor,
+    SPVertex,
+    SP1Triangle,
+    SP2Triangles,
+    SPEndDisplayList
+};
+
 // Global parsing state
 n64Texture* lastTexture = NULL;
 
@@ -185,6 +440,50 @@ static void* _dlist_commandstring(DListCName c, int size, ...)
 
 static void* _dlist_commandbinary(DListCName c, int size, ...)
 {
+    char supported = 0;
+    va_list args;
+    char strbuff[STRBUF_SIZE];
+    va_start(args, size);
+    int* binarydata;
+
+    // Check the command is supported
+    for (int i=0; i<sizeof(supported_binary)/sizeof(supported_binary[0]); i++)
+    {
+        if (supported_binary[i] == c)
+        {
+            supported = 1;
+            break;
+        }
+    }
+    if (!supported)
+    {
+        sprintf(strbuff, "Unsupported Binary DL command %s", commands_f3dex2[c].name);
+        terminate(strbuff);
+    }
+
+    // Go through each argument
+    binarydata = (int*)malloc(sizeof(int)*(size+1));
+    binarydata[0] = c;
+    for (int i=0; i<size; i++)
+    {
+        char* arg = va_arg(args, char*);
+        int argval;
+
+        // !!!!!!!!!!!!!!!!!!!!!!!
+        //binarydata[i+1]
+        // !!!!!!!!!!!!!!!!!!!!!!!
+
+        switch (c)
+        {
+            case SPVertex:
+                //if (i == 0)
+
+            default:
+                argval = atoi(arg);
+                break;
+        }
+    }
+    va_end(args);
     return 0;
 }
 
